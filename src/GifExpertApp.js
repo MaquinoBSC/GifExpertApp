12002f00d2465396f8d618ceb9624b50941c84b5
MaquinoBSC <martinare.ipn@gmail.com>
2022-06-06 14:25:57 -0500

    Creacion de una lista de categorias y una funcion para mutarla

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function GifExpertApp(){
+    const [ categories, setCategories ]= useState( ['One Punch', 'Samurai X', 'Dragon Ball'] ); 
+
+    const handleAdd= ()=> {
+        const name= "Hora de Aventura";
+        setCategories([ ...categories, name ]);
+    }
+
     return (
         <>
             <h2> GifExpertApp </h2>
             <hr />
+
+            <button onClick={ ()=> handleAdd() }> Agregar </button>
+            <ol>
+                {
+                    categories.map(( category )=> {
+                        return (
+                            <li key={ category }>{ category }</li>
+                        )
+                    })
+                }
+            </ol>
         </>
     )
 }
