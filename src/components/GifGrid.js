6d38611d62aaca750865be79b106ee954f761747
MaquinoBSC <martinare.ipn@gmail.com>
2022-06-07 18:27:10 -0500

    Crear helper GetGifs para optimizar el componente GifGrid

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,26 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { getGifs } from '../helpers/GetGifs';
 import GifGridItem from './GifGridItem';
 
 export default function GifGrid({ category }){
     const [ images, setImages ]= useState( [] );
 
     useEffect(()=> {
-        getGifs();
-    }, []);
-    
-    const getGifs= async ()=> {
-        const url= 'https://api.giphy.com/v1/gifs/search?api_key=dbzpRyieekgFAjfSqOzWXw0FNB3FlG6F&q=Rick+and+Morty&limit=10';
-        const response= await fetch( url );
-        const { data }= await response.json();
-        const gifs= data.map(( img )=> {
-            return {
-                'id': img.id,
-                'title': img.title,
-                'url': img.images.downsized_medium.url,
-            }
-        });
-        setImages( gifs );
-    }
+        getGifs(category)
+            .then( imgs=> setImages(imgs));
+    }, [ category ]);
 
     return (
         <>
