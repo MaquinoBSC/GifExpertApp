f73c532b27b894040a981c72862e4e91b084ff39
MaquinoBSC <martinare.ipn@gmail.com>
2025-02-12 20:39:47 -0600

    chore: creation of categories list

--- src/GifExertApp.jsx
+++ src/GifExertApp.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 
 const GifExpertApp = () => {
+    const [ categories, setCategories ] = useState(['js', 'node']);
+
     return (
         <>
             <h1>Gif Expert App</h1>
+
+            <ol>
+                { categories.map((category) => <li key={ category }>{ category }</li>)}
+            </ol>
         </>
     )
 }
